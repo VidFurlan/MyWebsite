95ce67b312aef15961a57ef6c96b1a296d372428
Vid Furlan <nalrufdiv@gmail.com>
2023-01-30 12:28:43 +0100

    Fixed topbar and auto generate it with Javascript

---
+++ Javascript/topbar.js
@@ -0,0 +1,8 @@
+const toGenerate = `
+<ul class="menu">
+<li><a href="/index.html">Home</a></li>
+<li><a href="https://github.com/VidDoingCode/MyWebsite" target="_blank">Github</a></li>
+<li><a href="">About</a></li>
+</ul>`;
+
+document.body.innerHTML = toGenerate;
